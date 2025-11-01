cd351334332deb291789b5a403a3519ccf0526ec
QuocChien142 <daoquocchien07082004@gmail.com>
2025-11-01 14:32:28 +0700

    update

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import myAlert from "~/myAlert";
+function Button() {
+    return(
+        <div>
+            <button onClick={myAlert}>Click me</button>
+        </div>
+    )
+}
+
+export default Button;
---
+++ src/myAlert.js
@@ -0,0 +1,5 @@
+const myAlert = () => {
+    alert('Hello anh em FA');
+};
+
+export default myAlert;
